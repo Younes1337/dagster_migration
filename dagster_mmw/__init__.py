5b2f19662afafcd154e9ebc17252a0ec7c826b85
Younes1337 <dn43.youn2019@gmail.com>
2025-06-22 19:24:08 +0100

    Adding profiles.yaml

--- dagster_mmw/__init__.py
+++ dagster_mmw/__init__.py
@@ -1,10 +1,19 @@
 # fmt: off
 from dagster import Definitions, load_assets_from_modules # type: ignore
-from .assets import test_asset, example_nightly_ingest_job, demo_nightly_pipeline
+from dagster_dbt import load_assets_from_dbt_project # type: ignore
+import os
+
+from .assets import test_asset
+from .jobs import ingetion_pipeline
 
 test_assets = load_assets_from_modules([test_asset])
 
+dbt_assets = load_assets_from_dbt_project(
+    project_dir=os.path.join(os.path.dirname(__file__), '..', 'transformations'),
+    profiles_dir=os.path.join(os.path.dirname(__file__), '..', 'transformations'),
+)
+
 defs = Definitions(
-    assets=test_assets,
-    jobs=[example_nightly_ingest_job, demo_nightly_pipeline],
+    assets=[*test_assets, *dbt_assets],
+    jobs=[ingetion_pipeline.nightly_ingest_job],
 )
