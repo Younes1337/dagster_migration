2f65ac88e3b2e899b0260bd62efef47a14d47079
Younes1337 <dn43.youn2019@gmail.com>
2025-06-22 14:20:06 +0100

    Adding dbt files

--- dagster_mmw/__init__.py
+++ dagster_mmw/__init__.py
@@ -1,11 +1,10 @@
 # fmt: off
 from dagster import Definitions, load_assets_from_modules # type: ignore
-from .assets import test_asset
-from .jobs import ingetion_pipeline
+from .assets import test_asset, example_nightly_ingest_job, demo_nightly_pipeline
 
 test_assets = load_assets_from_modules([test_asset])
 
 defs = Definitions(
-    assets=[*test_assets],
-    jobs=[ingetion_pipeline.nightly_ingest_job]
+    assets=test_assets,
+    jobs=[example_nightly_ingest_job, demo_nightly_pipeline],
 )
